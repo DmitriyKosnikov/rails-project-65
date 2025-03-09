a933e2ca2ce83c414f5fbc2896fa61cc8113b0f9
DmitriyKosnikov <kosnikov.dm@gmail.com>
2025-03-09 10:24:46 +0300

    fixing pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,20 +13,20 @@ IMAGES_NAMES = %w[restaurant_1.jpg restaurant_2.jpg restaurant_3.jpg restaurant_
 #   )
 # end
 
-100.times do
-  bulletin = Bulletin.create(
-    title: Faker::Restaurant.name,
-    description: Faker::Restaurant.description,
-    user_id: User.all.sample.id,
-    category_id: Category.all.sample.id,
-    state: Bulletin.aasm.states.sample.name
-  )
+# 100.times do
+#   bulletin = Bulletin.create(
+#     title: Faker::Restaurant.name,
+#     description: Faker::Restaurant.description,
+#     user_id: User.all.sample.id,
+#     category_id: Category.all.sample.id,
+#     state: Bulletin.aasm.states.sample.name
+#   )
 
-  name_img = IMAGES_NAMES.sample
-  bulletin.image.attach(
-    io: File.open(Rails.root.join("seeds/images/#{name_img}").to_s),
-    filename: name_img,
-    content_type: 'image/jpeg'
-  )
-  bulletin.save
-end
+#   name_img = IMAGES_NAMES.sample
+#   bulletin.image.attach(
+#     io: File.open(Rails.root.join("seeds/images/#{name_img}").to_s),
+#     filename: name_img,
+#     content_type: 'image/jpeg'
+#   )
+#   bulletin.save
+# end
